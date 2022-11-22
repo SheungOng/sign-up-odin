4f48890a9922b90a80058a21c7ece8e6ffb0ca71
sheung.ong <sheung.ong@osky.com.au>
2022-11-22 13:40:58 +0800

    Add HTML for create account and JS for pass validation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,21 @@
 var check = function() {
+    var passwordInput = document.getElementById('password');
+    var cpasswordInput = document.getElementById('confirm-password');
     var password = document.getElementById('password').value;
     var cpassword = document.getElementById('confirm-password').value;
-    if (password == cpassword || password.length || cpassword.length) {
-        password.style.borderColor = 'green';
-        cpassword.style.borderColor = 'green';
+    if (password == cpassword && password.length && cpassword.length) {
+        passwordInput.style.borderColor = 'green';
+        cpasswordInput.style.borderColor = 'green';
         document.getElementById('message').innerHTML = '* Passwords match!';
         document.getElementById('message').style.color = 'green';
-    } else {
+    } else if(password !== cpassword && password.length && cpassword.length) {
         document.getElementById('message').style.color = 'red';
-        password.style.borderColor = 'red';
-        cpassword.style.borderColor = 'red';
+        passwordInput.style.borderColor = 'red';
+        cpasswordInput.style.borderColor = 'red';
         document.getElementById('message').innerHTML = '* Passwords do not match';
+    } else {
+        passwordInput.style.borderColor = '#ced4da';
+        cpasswordInput.style.borderColor = '#ced4da';
+        document.getElementById('message').innerHTML = '';
     }
 }
